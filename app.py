322f81c7607052c90472e17a43015c43967d5469
xs3rgii <roales.sergio04@gmail.com>
2025-05-13 18:42:19 +0200

    formulario añadido

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
